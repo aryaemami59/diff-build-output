5e7d637a4284c30b3f5574b93d2188d58cc53b28
Arya Emami <aryaemami59@yahoo.com>
2026-04-03 18:59:09 -0500

    refactor: update type examples and improve comments in `typeHelpers.ts`

--- src/typeHelpers.ts
+++ src/typeHelpers.ts
@@ -14,36 +14,52 @@
  * <caption>Demonstrating `Omit` vs `DistributedOmit`</caption>
  *
  * ```ts
+ * import type { DistributedOmit } from "./typeHelpers.js";
+ *
  * type A = {
- *   discriminant: 'A';
- *   foo: string;
  *   a: number;
+ *   discriminant: "A";
+ *   foo: string;
  * };
  *
  * type B = {
- *   discriminant: 'B';
- *   foo: string;
  *   b: string;
+ *   discriminant: "B";
+ *   foo: string;
  * };
  *
  * type Union = A | B;
  *
- * type OmittedUnion = Omit<Union, 'foo'>;
- * // => { discriminant: 'A' | 'B' }
+ * const omittedUnion: Omit<Union, "foo"> = {
+ *   discriminant: "A",
+ * };
  *
- * const omittedUnion: OmittedUnion = createOmittedUnion();
- *
- * if (omittedUnion.discriminant === 'A') {
+ * if (omittedUnion.discriminant === "A") {
  *   // We would like to narrow `omittedUnion`'s type to `A` here,
  *   // but we can't because `Omit` doesn't distribute over unions.
  *
+ *   // @ts-expect-error
  *   omittedUnion.a;
- *   // => Error: Property 'a' does not exist on type '{ discriminant: "A" | "B" }'
+ *   // => ❌ Error: Property 'a' does not exist on type 'Omit<Union, "foo">'.
+ * }
+ *
+ * const distributedOmittedUnion: DistributedOmit<Union, "foo"> = {
+ *   a: 123,
+ *   discriminant: "A",
+ * };
+ *
+ * if (distributedOmittedUnion.discriminant === "A") {
+ *   // We can successfully narrow `distributedOmittedUnion`'s type to `A` here,
+ *   // because `DistributedOmit` distributes over unions.
+ *
+ *   distributedOmittedUnion.a;
+ *   // => ✅ OK
  * }
  * ```
  *
  * @template ObjectType - The base object or union type to omit properties from.
  * @template KeyType - The keys of {@linkcode ObjectType} to omit.
+ * @internal
  */
 export type DistributedOmit<
   ObjectType,
@@ -66,57 +82,93 @@ export type DistributedOmit<
  * <caption>Demonstrating `Pick` vs `DistributedPick`</caption>
  *
  * ```ts
+ * import type { DistributedPick } from "./typeHelpers.js";
+ *
  * type A = {
- *   discriminant: 'A';
+ *   discriminant: "A";
+ *   extraneous: boolean;
  *   foo: {
  *     bar: string;
  *   };
- *   extraneous: boolean;
  * };
  *
  * type B = {
- *   discriminant: 'B';
+ *   discriminant: "B";
+ *   extraneous: boolean;
  *   foo: {
  *     baz: string;
  *   };
- *   extraneous: boolean;
  * };
  *
  * // Notice that `foo.bar` exists in `A` but not in `B`.
  *
  * type Union = A | B;
  *
- * type PickedUnion = DistributedPick<Union, 'discriminant' | 'foo'>;
+ * const pickedUnion: Pick<Union, "discriminant" | "foo"> = {
+ *   discriminant: "A",
+ *   foo: {
+ *     bar: "",
+ *   },
+ * };
  *
- * declare const pickedUnion: PickedUnion;
+ * if (pickedUnion.discriminant === "A") {
+ *   // We would like to narrow to `A` here,
+ *   // but we can't because `Pick` doesn't distribute over unions.
  *
- * if (pickedUnion.discriminant === 'A') {
+ *   // @ts-expect-error
  *   pickedUnion.foo.bar;
- *    //=> OK
+ *   //=> ❌ Error: Property 'bar' does not exist on type '{ bar: string; } | { baz: string; }'.
  *
  *   // @ts-expect-error
  *   pickedUnion.extraneous;
- *   //=> Error: Property `extraneous` does not exist on type `Pick<A, 'discriminant' | 'foo'>`.
+ *   //=> ❌ Error: Property 'extraneous' does not exist on type 'Pick<Union, "discriminant" | "foo">'.
  *
  *   // @ts-expect-error
  *   pickedUnion.foo.baz;
- *   //=> Error: `bar` is not a property of `{ discriminant: 'A'; a: string }`.
+ *   // => ❌ Error: Property 'baz' does not exist on type '{ bar: string; } | { baz: string; }'.
+ * }
+ *
+ * const distributedPickedUnion: DistributedPick<Union, "discriminant" | "foo"> = {
+ *   discriminant: "A",
+ *   foo: {
+ *     bar: "",
+ *   },
+ * };
+ *
+ * if (distributedPickedUnion.discriminant === "A") {
+ *   // Narrowing works correctly because the pick is applied per union member.
+ *
+ *   distributedPickedUnion.foo.bar;
+ *   // => ✅ OK
+ *
+ *   // @ts-expect-error
+ *   distributedPickedUnion.extraneous;
+ *   //=> ❌ Error: Property 'extraneous' does not exist on type 'Pick<A, "discriminant" | "foo">'.
+ *
+ *   // @ts-expect-error
+ *   distributedPickedUnion.foo.baz;
+ *   //=> ❌ Error: Property 'baz' does not exist on type '{ bar: string; }'.
  * }
  * ```
  *
  * @template ObjectType - The base object or union type to pick properties from.
  * @template KeyType - The keys of {@linkcode ObjectType} to pick.
+ * @internal
  */
 export type DistributedPick<
   ObjectType,
   KeyType extends keyof ObjectType,
-> = ObjectType extends unknown ? Pick<ObjectType, KeyType> : never
+> = ObjectType extends unknown
+  ? Pick<ObjectType, Extract<KeyType, keyof ObjectType>>
+  : never
 
 /**
  * An alias for type **`{}`**. Represents any value that is not
  * **`null`** or **`undefined`**. It is mostly used for semantic purposes to
  * help distinguish between an empty object type and **`{}`**
  * as they are not the same.
+ *
+ * @internal
  */
 export type AnyNonNullishValue = NonNullable<unknown>
 
@@ -128,42 +180,49 @@ export type AnyNonNullishValue = NonNullable<unknown>
  * <caption>Basic usage</caption>
  *
  * ```ts
+ * import type { Simplify } from "./typeHelpers.js";
+ *
  * interface SomeInterface {
- *   foo: number;
  *   bar?: string;
  *   baz: number | undefined;
- * };
+ *   foo: number;
+ * }
  *
  * type SomeType = {
- *   foo: number;
  *   bar?: string;
  *   baz: number | undefined;
+ *   foo: number;
  * };
  *
- * const literal = { foo: 123, bar: 'hello', baz: 456 };
+ * const literal = {
+ *   bar: "hello",
+ *   baz: 456,
+ *   foo: 123,
+ * } as const satisfies SomeType satisfies SomeInterface;
+ *
  * const someType: SomeType = literal;
  * const someInterface: SomeInterface = literal;
  *
- * declare function fn(object: Record<string, unknown>): void;
+ * function fn(object: Record<string, unknown>): void {
+ *   console.log(object);
+ * }
  *
- * fn(literal); // Good: literal object type is sealed
- * fn(someType); // Good: type is sealed
+ * fn(literal); // ✅ Good: literal object type is sealed
+ * fn(someType); // ✅ Good: type is sealed
  * // @ts-expect-error
- * fn(someInterface); // Error: Index signature for type 'string' is missing in type 'someInterface'. Because `interface` can be re-opened
- * fn(someInterface as Simplify<SomeInterface>); // Good: transform an `interface` into a `type`
+ * fn(someInterface); // ❌ Error: Index signature for type 'string' is missing in type 'SomeInterface'. Because `interface` can be re-opened
+ * fn(someInterface as Simplify<SomeInterface>); // ✅ Good: transform an `interface` into a `type`
  * ```
  *
  * @template BaseType - The type to simplify.
  *
  * @see {@link https://github.com/sindresorhus/type-fest/blob/2300245cb6f0b28ee36c2bb852ade872254073b8/source/simplify.d.ts Source}
  * @see {@link https://github.com/microsoft/TypeScript/issues/15300 | TypeScript Issue}
+ * @internal
  */
-export type Simplify<BaseType> = BaseType extends
-  | ((...args: never[]) => unknown)
-  | (abstract new (...args: never[]) => unknown)
-  | (new (...args: never[]) => unknown)
+export type Simplify<BaseType> = BaseType extends (...args: never[]) => unknown
   ? BaseType
-  : AnyNonNullishValue & {
+  : NonNullable<unknown> & {
       [KeyType in keyof BaseType]: BaseType[KeyType]
     }
 
